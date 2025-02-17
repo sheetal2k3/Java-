8ad1e9e184400093a2e7fb013fb62e00c5d0363d
Sheetal k <107554096+sheetal2k3@users.noreply.github.com>
2025-02-17 19:55:27 +0530

    Add files via upload

---
+++ AverageCalculator.java
@@ -0,0 +1,9 @@
+public class AverageCalculator{
+ public static double averageofthreenumbers(double a,double b,double c){
+ return (a+b+c)/3;
+}
+ public static void main(String[] args){
+ double result = averageofthreenumbers(15,25,35);
+ System.out.println(result);
+}
+}
