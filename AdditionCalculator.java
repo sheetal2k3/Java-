f521292074002bc156e35c6188da50634f165486
Sheetal k <107554096+sheetal2k3@users.noreply.github.com>
2025-02-17 20:00:42 +0530

    Add files via upload

---
+++ AdditionCalculator.java
@@ -0,0 +1,9 @@
+public class AdditionCalculator{
+  public static int addTwoNumbers(int a ,int b){
+  return (a+b);
+  }
+  public static void main(String[] args){
+  int result = addTwoNumbers(10,20);
+  System.out.println(result);
+  }
+}
